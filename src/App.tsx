00537dbf9dbdbc9c6b194d243ea8866c552dd56f
aemorozov <mclenina19@gmail.com>
2023-03-16 17:47:11 +0400

    chore: add context for nodal

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import { Loader } from './components/Loader';
 import { Modal } from './components/Modal';
 import { Product } from './components/Product';
 import { useProducts } from './hooks/products';
-import { useState } from 'react'
+import { useContext } from 'react'
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
 
   const { products, loading, error, addProduct } = useProducts()
 
-  const [modal, setModal] = useState(false)
+  const { modal, open, close } = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    close()
     addProduct(product)
   }
 
@@ -25,13 +26,13 @@ function App() {
       { error && <ErrorMassage error={ error } /> }
       { products.map( product => <Product product={product} key={product.id} />) }
 
-      { modal && <Modal title="Create new product" onClose={ () => setModal(false) }>
+      { modal && <Modal title="Create new product" onClose={ close }>
         <CreateProduct onCreate={ createHandler } />
       </Modal> }
 
       <button 
         className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 pb-2.5 pt-1'
-        onClick={ () => setModal(true) }  
+        onClick={ open }  
       >+</button>
       
     </div>
